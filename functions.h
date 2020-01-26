5593cb67cbcd9b07b99a71f1a73a038e454a90dc
Taumah <ttresgots@gmail.com>
2020-01-26 11:57:11 +0100

    base du programme , prototypes et prog modulaire

--- functions.h
+++ functions.h
@@ -17,9 +17,11 @@ typedef struct ppm_image_t{
 }ppm_image_t;
 
 
+
 __uint8_t img_blue(const pixel_t *p);
 __uint8_t img_red(const pixel_t *p);
 __uint8_t img_green(const pixel_t *p);
 
+pixel_t pixel_new(__uint8_t red, __uint8_t green, __uint8_t blue);
 
 bool cmp_pix(const pixel_t *p , const pixel_t *p2);
