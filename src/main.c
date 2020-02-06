67d2c66dc14dd3ddac6c970bd24ad709d4df9f6c
antoineThiel <antoine.thiel@hotmail.fr>
2020-02-06 17:12:41 +0100

    exam

--- src/main.c
+++ src/main.c
@@ -3,13 +3,13 @@
 
 int main(/*int argc, char** argv*/){
     
-    ppm_image_t picture = ppm_new("small.ppm");
+    ppm_image_t picture = ppm_new("original.ppm");
     ppm_image_t copy = ppm_revert(&picture);
     // pixel_t test = {20, 30 ,40};
 
     ppm_print(&copy);
 
-    ppm_file_from_pic(&picture , "picture.ppm");
+    ppm_file_from_pic(&picture , "original.ppm");
     ppm_file_from_pic(&copy , "copy.ppm");
 
     printf("il y a %lu pixel(s) noirs \n\n\n\n" , ppm_black_counter(&copy));
